8ba5df3bc908d11761ae327f6d4b2ea952335af1
Adam Pratley <adampratley1@gmail.com>
2023-06-28 01:59:25 +0100

    lol almost lost everything

--- src/app.rs
+++ src/app.rs
@@ -9,21 +9,20 @@ pub fn App(cx: Scope) -> impl IntoView {
 
     view! {
         cx,
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
-
-        // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Test Leptos Webapp"/>
 
         // content for this welcome page
         <Router>
+            <NavigationBar>
+            </NavigationBar>
             <main>
-                <Routes>
-                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
-                    <Route path="/test" view=|cx| view! { cx, <TestPage/> }/>
-                </Routes>
+                <body class="bg-black">
+                    <Routes>
+                        <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                        <Route path="/test" view=|cx| view! { cx, <TestPage/> }/>
+                    </Routes>
+                </body>
             </main>
         </Router>
     }
@@ -39,8 +38,8 @@ fn HomePage(cx: Scope) -> impl IntoView {
     view! { cx,
         <div class="flex items-center">
             <div class="mx-auto">
-                <h1 class="mx-auto">"Welcome to Leptos!"</h1>
-                <div class="flex justify-center">
+                <h1 class="mx-auto text-5xl text-amber-600">"Welcome to Web App!"</h1>
+                <div class="flex justify-center mt-4 bg-black">
                     <button class="bg-amber-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg" on:click=on_click>"Click Me: " {count}</button>
                 </div>
             </div>
@@ -52,6 +51,20 @@ fn HomePage(cx: Scope) -> impl IntoView {
 fn TestPage(cx: Scope) -> impl IntoView {
 
     view!{ cx,
-        <h1>"Test Page, This means routing works!"</h1>
+        <h1 clas="text-white">"Test Page, This means routing works!"</h1>
+    }
+}
+
+#[component]
+fn NavigationBar(cx: Scope) -> impl IntoView {
+    
+    view!{cx, 
+     <div class="max-auto flex h-[5.5rem] max-w-[1344px] w-full px-5 sm:px-10 bg-black items-center">
+        <a href="/" class="text-lg text-white">"Web App :)"</a>
+        <div class="flex flex-col gap-4 xl:flex-row xl:gap-8 text-slate-400 mx-10">
+            <a href="/test">"Route1"</a>
+            <a href="/test">"Route2"</a>
+        </div>
+     </div>
     }
 }
